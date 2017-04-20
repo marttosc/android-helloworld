9d09d36288a14200a33fe1ce412dd96f7114b639
Gustavo Marttos <marttosc@gmail.com>
2017-04-19 21:11:23 -0300

    Create a button to logout.

--- app/src/main/java/me/marttos/helloworld/HomeActivity.java
+++ app/src/main/java/me/marttos/helloworld/HomeActivity.java
@@ -5,13 +5,15 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import me.marttos.helloworld.model.User;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private Button btnLogout;
     private SharedPreferences prefs;
 
     @Override
@@ -23,6 +25,14 @@ public class HomeActivity extends AppCompatActivity {
                 "session", Context.MODE_PRIVATE
         );
 
+        btnLogout = (Button) findViewById(R.id.btnSair);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goLogin();
+            }
+        });
+
         boolean logged = prefs.getBoolean("logged", false);
         String email_logged = prefs.getString("email", "");
 
@@ -30,12 +40,7 @@ public class HomeActivity extends AppCompatActivity {
 
         if (email_logged.contentEquals("") || !logged || user == null)
         {
-            Intent intent = new Intent(this, MainActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-
-            resetSession();
-
-            startActivity(intent);
+            goLogin();
         }
         else
         {
@@ -50,5 +55,15 @@ public class HomeActivity extends AppCompatActivity {
         editor.putBoolean("logged", false);
         editor.putString("email", null);
         editor.commit();
+    }
+
+    private void goLogin()
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        resetSession();
+
+        startActivity(intent);
     }
 }
